30c9e60bcd9012cddd79d34c690251ec0796f6d2
kampachi0096 <kanchann033@gmail.com>
2024-12-08 15:45:15 +0900

    first commit

---
+++ ghost/ghost/ghostApp.swift
@@ -0,0 +1,17 @@
+//
+//  ghostApp.swift
+//  ghost
+//
+//  Created by kantayamamoto   on 2024/12/08.
+//
+
+import SwiftUI
+
+@main
+struct ghostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ghost/ghostTests/ghostTests.swift
@@ -0,0 +1,17 @@
+//
+//  ghostTests.swift
+//  ghostTests
+//
+//  Created by kantayamamoto   on 2024/12/08.
+//
+
+import Testing
+@testable import ghost
+
+struct ghostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
